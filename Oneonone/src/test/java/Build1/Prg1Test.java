da77e1d6e9dd0a5483b9a8b63af8536c0ce9a87e
shilpag1130 <shilpagajare11@gmail.com>
2023-06-02 13:21:12 +0530

    commit3

--- Oneonone/src/test/java/Build1/Prg1Test.java
+++ Oneonone/src/test/java/Build1/Prg1Test.java
@@ -1,5 +1,6 @@
 package Build1;
 
+import org.testng.Reporter;
 import org.testng.annotations.Test;
 
 public class Prg1Test {
@@ -8,9 +9,11 @@ public class Prg1Test {
 	public void demo1()
 	{
 		System.out.println("hiii");
+		Reporter.log("hello",true);
 	}
 	//hiii
 	//hi.
 	//ok.
+	//hey
 
 }
