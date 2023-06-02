11f63eb444b8b7a5e56e2ffe9fd43d0be46a1f78
shilpag1130 <shilpagajare11@gmail.com>
2023-06-02 15:44:22 +0530

    commit4

---
+++ Oneonone/src/test/java/Build1/Prg3.java
@@ -0,0 +1,14 @@
+package Build1;
+
+import org.testng.annotations.Test;
+
+public class Prg3 {
+	
+	
+	@Test
+	public void demo()
+	{
+		System.out.println("hey u dyr");
+	}
+
+}
---
+++ Oneonone/src/test/java/Build1/prg2.java
@@ -0,0 +1,13 @@
+package Build1;
+
+import org.testng.annotations.Test;
+
+public class prg2 {
+	
+	@Test
+	public void test()
+	{
+		System.out.println("hey u dyr");
+	}
+
+}
